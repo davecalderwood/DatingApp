65f23d026faf53d051f23c2535139de5a8ca7eeb
Dave Calderwood <43446756+davecalderwood@users.noreply.github.com>
2020-10-01 12:01:55 -0400

    Added UI ssl components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  // tslint:disable-next-line: typedef
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  // tslint:disable-next-line: typedef
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(resposne => {
+      this.users = resposne;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
