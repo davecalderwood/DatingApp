47d83d5f6d4b6441166ccb3f0cc79aaa19fac1b1
Dave Calderwood <43446756+davecalderwood@users.noreply.github.com>
2020-10-24 13:06:49 -0400

    Error handling

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var error = _context.Users.Find(-1);
+
+            if(error == null) { return NotFound(); }
+
+            return Ok(error);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var error = _context.Users.Find(-1);
+
+            var errorReturn = error.ToString();
+
+            return errorReturn;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request");
+        }
+        
+    }
+}
