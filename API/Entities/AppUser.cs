2ab84a2953265596b3ba4f9e697d3078ca079530
Dave Calderwood <43446756+davecalderwood@users.noreply.github.com>
2020-10-28 13:32:33 -0400

    automapper

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,7 +8,6 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
@@ -23,9 +22,9 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        public int GetAge() 
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge() 
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
